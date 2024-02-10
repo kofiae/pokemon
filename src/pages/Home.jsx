9edeee0794ce8a536b4671a29a401da042d1c568
Olubusola ODUFEJO OGOE <olubusola.odufejo-ogoe@etu.u-bordeaux.fr>
2024-02-10 03:02:03 +0100

    add: types et stats

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,6 +5,7 @@ import Bulbasaur from '../asset/images/poke/bulbasaur.png';
 import Deerling from '../asset/images/poke/deerling.png';
 import Persian from '../asset/images/poke/persian.png';
 import Ponyta from '../asset/images/poke/ponyta.png';
+import PonytaStats from '../asset/images/poke/ponytastats.png';
 import Taillow from '../asset/images/poke/taillow.png';
 import Tepig from '../asset/images/poke/tepig.png';
 
@@ -25,6 +26,7 @@ function Home() {
           <img src={Persian} alt='Persian' className='persian' />
           <img src={Ponyta} alt='Ponyta' className='ponyta' />
           <img src={Tepig} alt='Tepig' className='tepig' />
+          <img src={PonytaStats} alt='PonytaStats' className='ponytastats' />
 
         </div>
     </div>
