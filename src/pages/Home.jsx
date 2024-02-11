94cef975c883fe972eb92b2a0c94c6859091a82d
Olubusola ODUFEJO OGOE <olubusola.odufejo-ogoe@etu.u-bordeaux.fr>
2024-02-11 19:36:08 +0100

    add: anim pokemon

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,5 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
+import { motion } from 'framer-motion';
 import '../asset/Home.css';
 import Logo from '../asset/images/logopokemon.png';
 import Background from '../asset/images/landscape.jpg';
@@ -11,40 +12,210 @@ import Taillow from '../asset/images/poke/taillow.png';
 import Tepig from '../asset/images/poke/tepig.png';
 
 function Home() {
-  const [ponytaStatsVisible, setPonytaStatsVisible] = useState(false);
+  const [isVisibleLogo, setIsVisibleLogo] = useState(false);
+  const [isVisibleTaillow, setIsVisibleTaillow] = useState(false);
+  const [isVisibleBulbasaur, setIsVisibleBulbasaur] = useState(false);
+  const [isVisibleDeerling, setIsVisibleDeerling] = useState(false);
+  const [isVisiblePersian, setIsVisiblePersian] = useState(false);
+  const [isVisiblePonyta, setIsVisiblePonyta] = useState(false);
+  const [isVisibleTepig, setIsVisibleTepig] = useState(false);
+  const [isVisiblePonytaStats, setIsVisiblePonytaStats] = useState(false);
+
+  const logoRef = useRef(null);
+  const taillowRef = useRef(null);
+  const bulbasaurRef = useRef(null);
+  const deerlingRef = useRef(null);
+  const persianRef = useRef(null);
+  const ponytaRef = useRef(null);
+  const tepigRef = useRef(null);
+  const ponytaStatsRef = useRef(null);
+
+  useEffect(() => {
+    const observerLogo = new IntersectionObserver(
+      ([entry]) => {
+        setIsVisibleLogo(entry.isIntersecting);
+      },
+      {
+        root: null,
+        rootMargin: '0px',
+        threshold: 0.7
+      }
+    );
+    const observerTaillow = new IntersectionObserver(
+      ([entry]) => {
+        setIsVisibleTaillow(entry.isIntersecting);
+      },
+      {
+        root: null,
+        rootMargin: '0px',
+        threshold: 0.7
+      }
+    );
+
+    const observerBulbasaur = new IntersectionObserver(
+      ([entry]) => {
+        setIsVisibleBulbasaur(entry.isIntersecting);
+      },
+      {
+        root: null,
+        rootMargin: '0px',
+        threshold: 0.7
+      }
+    );
+
+    const observerDeerling = new IntersectionObserver(
+      ([entry]) => {
+        setIsVisibleDeerling(entry.isIntersecting);
+      },
+      {
+        root: null,
+        rootMargin: '0px',
+        threshold: 0.7
+      }
+    );
+
+    const observerPersian = new IntersectionObserver(
+      ([entry]) => {
+        setIsVisiblePersian(entry.isIntersecting);
+      },
+      {
+        root: null,
+        rootMargin: '0px',
+        threshold: 0.7
+      }
+    );
+
+    const observerPonyta = new IntersectionObserver(
+      ([entry]) => {
+        setIsVisiblePonyta(entry.isIntersecting);
+      },
+      {
+        root: null,
+        rootMargin: '0px',
+        threshold: 0.7
+      }
+    );
+
+    const observerTepig = new IntersectionObserver(
+      ([entry]) => {
+        setIsVisibleTepig(entry.isIntersecting);
+      },
+      {
+        root: null,
+        rootMargin: '0px',
+        threshold: 0.7
+      }
+    );
+
+    const observerPonytaStats = new IntersectionObserver(
+      ([entry]) => {
+        setIsVisiblePonytaStats(entry.isIntersecting);
+      },
+      {
+        root: null,
+        rootMargin: '0px',
+        threshold: 0.7
+      }
+    );
+
+    if (logoRef.current) {
+      observerLogo.observe(logoRef.current);
+    }
+
+    if (taillowRef.current) {
+      observerTaillow.observe(taillowRef.current);
+    }
+
+    if (bulbasaurRef.current) {
+      observerBulbasaur.observe(bulbasaurRef.current);
+    }
+
+    if (deerlingRef.current) {
+      observerDeerling.observe(deerlingRef.current);
+    }
+
+    if (persianRef.current) {
+      observerPersian.observe(persianRef.current);
+    }
+
+    if (ponytaRef.current) {
+      observerPonyta.observe(ponytaRef.current);
+    }
+
+    if (tepigRef.current) {
+      observerTepig.observe(tepigRef.current);
+    }
+
+    if (ponytaStatsRef.current) {
+      observerPonytaStats.observe(ponytaStatsRef.current);
+    }
+
+    return () => {
+      if (taillowRef.current) {
+        observerTaillow.unobserve(taillowRef.current);
+      }
+      if (bulbasaurRef.current) {
+        observerBulbasaur.unobserve(bulbasaurRef.current);
+      }
+      if (deerlingRef.current) {
+        observerDeerling.unobserve(deerlingRef.current);
+      }
+      if (persianRef.current) {
+        observerPersian.unobserve(persianRef.current);
+      }
+      if (ponytaRef.current) {
+        observerPonyta.unobserve(ponytaRef.current);
+      }
+      if (ponytaStatsRef.current) {
+        observerPonytaStats.unobserve(ponytaStatsRef.current);
+      }
+    };
+  }, []);
 
   const togglePonytaStatsVisibility = () => {
-    setPonytaStatsVisible(!ponytaStatsVisible);
+    setIsVisiblePonytaStats(!isVisiblePonytaStats);
   };
 
   const openLink = () => {
-    window.location.href = '/types'
-  }
+    window.location.href = '/types';
+  };
 
   return (
     <div className='background-container'>
       <img src={Background} alt='Background' className='background-image' />
       <div className='logo-container'>
-        <img src={Logo} alt='Logo' className='logo' />
+        <motion.img src={Logo} alt='Logo' className='logo' style={{ opacity: isVisibleLogo ? 1 : 0, transition: 'opacity 1s' }} ref={logoRef}/>
       </div>
 
       <div className='pokemon-container'>
-        <img src={Taillow} alt='Taillow' className='taillow' />
-        <img src={Bulbasaur} alt='Bulbasaur' className='bulbasaur' />
-        <img src={Deerling} alt='Deerling' className='deerling' />
-        <img src={Persian} alt='Persian' className='persian' />
+          <motion.img src={Taillow} alt='Taillow' className='taillow' style={{ opacity: isVisibleTaillow ? 1 : 0, transition: 'opacity 1s' }} ref={taillowRef}/>
+          <motion.img src={Bulbasaur} alt='Bulbasaur' className='bulbasaur' style={{ opacity: isVisibleBulbasaur ? 1 : 0, transition: 'opacity 1s' }} ref={bulbasaurRef}/>
+          <motion.img src={Deerling} alt='Deerling' className='deerling' style={{ opacity: isVisibleDeerling ? 1 : 0, transition: 'opacity 1s' }} ref={deerlingRef}/>
+          <motion.img src={Persian} alt='Persian' className='persian' style={{ opacity: isVisiblePersian ? 1 : 0, transition: 'opacity 1s' }} ref={persianRef}/>
+          <motion.img src={Tepig} alt='Tepig' className='tepig' style={{ opacity: isVisibleTepig ? 1 : 0, transition: 'opacity 1s' }} ref={tepigRef}/>
+
         <a>
-          <img
+          <motion.img
             src={Ponyta}
             alt='Ponyta'
             className='ponyta'
             onClick={togglePonytaStatsVisibility}
+            style={{ opacity: isVisiblePonyta ? 1 : 0, transition: 'opacity 1s' }}
+            ref={ponytaRef}
           />
         </a>
-        <img src={Tepig} alt='Tepig' className='tepig' />
-        {ponytaStatsVisible && (
-          <a><img src={PonytaStats} alt='PonytaStats' className='ponytastats' onClick={openLink}/></a>
-        )}
+        <motion.div animate={{ opacity: isVisiblePonytaStats ? 1 : 0 }} initial={{ opacity: 0 }} ref={ponytaStatsRef}>
+          {isVisiblePonytaStats && (
+            <a>
+              <img
+                src={PonytaStats}
+                alt='PonytaStats'
+                className='ponytastats'
+                onClick={openLink}
+              />
+            </a>
+          )}
+        </motion.div>
       </div>
     </div>
   );
