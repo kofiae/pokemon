43ffebe54755f2ff426376f7175c2513b52f57c9
Olubusola ODUFEJO OGOE <olubusola.odufejo-ogoe@etu.u-bordeaux.fr>
2024-02-12 17:36:25 +0100

    RE: THE END

--- src/charts/StatTypePresence.jsx
+++ src/charts/StatTypePresence.jsx
@@ -1,51 +1,76 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import HighchartsReact from "highcharts-react-official";
 import Highcharts from "highcharts";
 import Papa from "papaparse";
-import { type } from "@testing-library/user-event/dist/type";
 
 function StatTypePresence() {
 
     // Récupérer les données des types de pokemons dans .../Smogon.csv
     const csvFilePath = require('./Smogon.csv');
-    const [data, setData] = React.useState([]);
-    const [types, setTypes] = React.useState({});
-    const [nbPokemon, setNbPokemon] = React.useState(0);
+    const [data, setData] = useState([]);
+    const [types, setTypes] = useState({});
+    const [nbPokemon, setNbPokemon] = useState(0);
 
-    const queryParameters = new URLSearchParams(window.location.search)
-    const tier = queryParameters.get('tier');
-
-    React.useEffect(() => {
+    useEffect(() => {
         Papa.parse(csvFilePath, {
             header: true,
             download: true,
             skipEmptyLines: true,
             complete: (result) => {
                 const data = result.data;
+                const typeCounts = {};
+                let totalPokemon = 0;
+
                 data.forEach((d) => {
-                        if (d.Type_1 in types) {
-                            types[d.Type_1] += 1;
-                        } else {
-                            types[d.Type_1] = 1;
-                        }
-                        if (d.Type_2 in types) {
-                            types[d.Type_2] += 1;
-                        }else if (d.Type_2.trim() !== ""){
-                            types[d.Type_2] = 1;
-                        }
-                        setNbPokemon((prev) => prev + 1);
+                    if (d.Type_1 in typeCounts) {
+                        typeCounts[d.Type_1] += 1;
+                    } else {
+                        typeCounts[d.Type_1] = 1;
+                    }
+                    if (d.Type_2 in typeCounts) {
+                        typeCounts[d.Type_2] += 1;
+                    } else if (d.Type_2.trim() !== "") {
+                        typeCounts[d.Type_2] = 1;
+                    }
+                    totalPokemon++;
                 });
-                setData(
-                    Object.keys(types).map((type) => ({
-                        Type: type,
-                        Presence: types[type],
-                    }))
-                );
-                console.log(data)
+
+                // Convert type counts to an array of objects
+                const typeData = Object.keys(typeCounts).map((type) => ({
+                    Type: type,
+                    Presence: typeCounts[type],
+                }));
+
+                // Sort the data by presence (descending order)
+                typeData.sort((a, b) => b.Presence - a.Presence);
+
+                setData(typeData);
+                setTypes(typeCounts);
+                setNbPokemon(totalPokemon);
             },
         });
     }, []);
 
+    const typeColors = {
+        'Normal': '#A8A878',
+        'Fire': '#F08030',
+        'Water': '#6890F0',
+        'Electric': '#F8D030',
+        'Grass': '#78C850',
+        'Ice': '#98D8D8',
+        'Fighting': '#C03028',
+        'Poison': '#A040A0',
+        'Ground': '#E0C068',
+        'Flying': '#A890F0',
+        'Psychic': '#F85888',
+        'Bug': '#A8B820',
+        'Rock': '#B8A038',
+        'Ghost': '#705898',
+        'Dragon': '#7038F8',
+        'Dark': '#705848',
+        'Steel': '#B8B8D0',
+        'Fairy': '#EE99AC'
+    };
 
     // Faire Pie chart pour les types de pokemons utilise types
     const options = {
@@ -57,7 +82,7 @@ function StatTypePresence() {
         },
         series: [
             {
-                data: Object.entries(types).map(([type, presence]) => ({ name: type, y: (presence/nbPokemon*100) })),
+                data: data.map(({ Type, Presence }) => ({ name: Type, y: (Math.round(Presence/nbPokemon*100)), color: typeColors[Type] })),
             },
         ],
     };
--- src/charts/StatTypePresenceParTier.jsx
+++ src/charts/StatTypePresenceParTier.jsx
@@ -1,17 +1,16 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import HighchartsReact from "highcharts-react-official";
 import Highcharts from "highcharts";
 import Papa from "papaparse";
-import { type } from "@testing-library/user-event/dist/type";
 
 function StatTypePrecenceParTier() {
 
     // Récupérer les données des types de pokemons dans .../Smogon.csv
     const csvFilePath = require('./Smogon.csv');
-    const [data, setData] = React.useState([]);
-    const [types, setTypes] = React.useState({});
-    const [nbPokemon, setNbPokemon] = React.useState(0);
-    const [tiers, setTiers] = React.useState([
+    const [data, setData] = useState([]);
+    const [types, setTypes] = useState({});
+    const [nbPokemon, setNbPokemon] = useState(0);
+    const [tiers, setTiers] = useState([
         "Uber",
         "OU",
         "UU",
@@ -25,39 +24,68 @@ function StatTypePrecenceParTier() {
         tier = "untiered";
     }
 
-    React.useEffect(() => {
+    useEffect(() => {
         Papa.parse(csvFilePath, {
             header: true,
             download: true,
             skipEmptyLines: true,
             complete: (result) => {
                 const data = result.data;
+                const typeCounts = {};
+                let totalPokemon = 0;
+
                 data.forEach((d) => {
                     if (d.Tier === tier) {
-                        if (d.Type_1 in types) {
-                            types[d.Type_1] += 1;
+                        if (d.Type_1 in typeCounts) {
+                            typeCounts[d.Type_1] += 1;
                         } else {
-                            types[d.Type_1] = 1;
+                            typeCounts[d.Type_1] = 1;
                         }
-                        if (d.Type_2 in types) {
-                            types[d.Type_2] += 1;
-                        }else if (d.Type_2.trim() !== ""){
-                            types[d.Type_2] = 1;
+                        if (d.Type_2 in typeCounts) {
+                            typeCounts[d.Type_2] += 1;
+                        } else if (d.Type_2.trim() !== "") {
+                            typeCounts[d.Type_2] = 1;
                         }
-                        setNbPokemon((prev) => prev + 1);
+                        totalPokemon++;
                     }
                 });
-                setData(
-                    Object.keys(types).map((type) => ({
-                        Type: type,
-                        Presence: types[type],
-                    }))
-                );
-                console.log(data)
+
+                // Convert type counts to an array of objects
+                const typeData = Object.keys(typeCounts).map((type) => ({
+                    Type: type,
+                    Presence: typeCounts[type],
+                }));
+
+                // Sort the typeData array based on presence
+                typeData.sort((a, b) => b.Presence - a.Presence);
+
+                setData(typeData);
+                setTypes(typeCounts);
+                setNbPokemon(totalPokemon);
             },
         });
     }, []);
 
+    const typeColors = {
+        'Normal': '#A8A878',
+        'Fire': '#F08030',
+        'Water': '#6890F0',
+        'Electric': '#F8D030',
+        'Grass': '#78C850',
+        'Ice': '#98D8D8',
+        'Fighting': '#C03028',
+        'Poison': '#A040A0',
+        'Ground': '#E0C068',
+        'Flying': '#A890F0',
+        'Psychic': '#F85888',
+        'Bug': '#A8B820',
+        'Rock': '#B8A038',
+        'Ghost': '#705898',
+        'Dragon': '#7038F8',
+        'Dark': '#705848',
+        'Steel': '#B8B8D0',
+        'Fairy': '#EE99AC'
+    };
 
     // Faire Pie chart pour les types de pokemons utilise types
     const options = {
@@ -69,7 +97,7 @@ function StatTypePrecenceParTier() {
         },
         series: [
             {
-                data: Object.entries(types).map(([type, presence]) => ({ name: type, y: (presence/nbPokemon*100) })),
+                data: data.map(({ Type, Presence }) => ({ name: Type, y: (Math.round(Presence / nbPokemon * 100)), color: typeColors[Type] })),
             },
         ],
     };
@@ -80,7 +108,6 @@ function StatTypePrecenceParTier() {
                 highcharts={Highcharts}
                 options={options}
             />
-            
         </div>
     );
 }
